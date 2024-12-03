5c56a72eeea4384d1ef35939ebabe1c5968cc045
J. Peng <ykttt@tuta.io>
2024-12-03 17:45:29 +0900

    bump to v1.0.1-a22

--- flake.nix
+++ flake.nix
@@ -13,10 +13,10 @@
     nixpkgs
   }: let
     system = "x86_64-linux";
-    version = "1.0.1-a.19";
+    version = "1.0.1-a.22";
     downloadUrl = {
       specific.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-      specific.sha256 = "1g7nq1yfaya97m43vnkjj1nd9g570viy8hj45c523hcyr1z92rjq";
+      specific.sha256 = "0anr79rdm62h5y37xa47rrrk32r9gnv04df4z7brc0hp4q83pxvi";
     };
     pkgs = import nixpkgs { inherit system; };
     runtimeLibs = with pkgs; [
